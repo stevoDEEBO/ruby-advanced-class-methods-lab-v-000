9b900df0c9e8771da731988df4149062d1a768f4
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-29 16:54:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name).nil? self.create_by_name(name) : self.find_by_name(name)
+    if self.find_by_name(name).nil?
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
     end
   end
 
